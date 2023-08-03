92ad26bed2868b87d9f79f049a87177e098d19a9
Fahrel Ardzaky E <fahrelardzaky@gmail.com>
2023-08-03 22:23:42 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.tjudul').slideDown(1500)
+    $('#produk1').fadeIn(2000)
+    $('#produk2').fadeIn(3000)
+    $('#produk3').fadeIn(4000)
+    $('#produk4').fadeIn(5000)
+    $('#produk5').fadeIn(2000)
+    $('#produk6').fadeIn(3000)
+    $('#produk7').fadeIn(4000)
+    $('#produk8').fadeIn(5000)
+
+    
+});
